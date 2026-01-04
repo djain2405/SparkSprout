a547498af468dcb928a13dcb6f8eafbe047e1ecf
Djain2405 <divyajain2405@gmail.com>
2026-01-03 21:06:22 -0800

    Initial Commit

---
+++ DayGlow/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DayGlow
+//
+//  Created by Divya Jain on 1/3/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DayGlow/DayGlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  DayGlowApp.swift
+//  DayGlow
+//
+//  Created by Divya Jain on 1/3/26.
+//
+
+import SwiftUI
+
+@main
+struct DayGlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
